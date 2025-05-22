2b299d0cb57dbe068300d54123e7235cca63693f
Rinkit Adhana <therinkit@gmail.com>
2025-05-23 02:20:16 +0530

    socket.io basic server created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,27 @@
-import express, { type Request, type Response } from "express"
+import { Server } from "socket.io"
+import { createServer } from "http"
+import type { IncomingMessage, ServerResponse } from "http"
 
-const app = express()
-const port = 8080
+const PORT = process.env.PORT || 3001
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello China!")
+const httpServer = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200)
+  res.end("webSocket Server is Running!")
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("Server is connected to socket!", socket.id)
+  socket.on("disconnect", () => {
+    console.log("Server is disconnected from socket!", socket.id)
+  })
+})
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
