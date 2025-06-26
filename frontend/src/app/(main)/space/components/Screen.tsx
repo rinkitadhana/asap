6b38f840937227929a4cb86dad9c994a21c197be
Rinkit Adhana <therinkit@gmail.com>
2025-06-26 18:39:12 +0530

    video interface updated

--- frontend/src/app/(main)/space/components/Screen.tsx
+++ frontend/src/app/(main)/space/components/Screen.tsx
@@ -3,11 +3,28 @@ import React from "react"
 const Screen = () => {
   return (
     <div className="flex h-full gap-2">
-      <div className="bg-background rounded-xl h-full w-full flex justify-center items-center border border-primary-border">
-        <div className="text-6xl font-bold font-cal">Gruz's Space</div>
+      <div className="bg-background rounded-xl h-full w-full flex justify-center items-center border border-primary-border relative">
+        <video 
+          id="user-1" 
+          autoPlay 
+          playsInline 
+          muted
+          className="w-full h-full object-cover rounded-xl"
+        />
+        <div className="absolute bottom-4 left-4 bg-primary-hover px-3 py-1.5 rounded-lg text-foreground text-sm">
+          You
+        </div>
       </div>
-      <div className="bg-background rounded-xl h-full w-full flex justify-center items-center border border-primary-border">
-        <div className="text-6xl font-bold font-cal">China No.1</div>
+      <div className="bg-background rounded-xl h-full w-full flex justify-center items-center border border-primary-border relative">
+        <video 
+          id="user-2" 
+          autoPlay 
+          playsInline
+          className="w-full h-full object-cover rounded-xl"
+        />
+        <div className="absolute bottom-4 left-4 bg-primary-hover px-3 py-1.5 rounded-lg text-foreground text-sm">
+          Remote User
+        </div>
       </div>
     </div>
   )
