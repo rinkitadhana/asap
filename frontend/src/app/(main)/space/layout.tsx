c0b426d0c1e475c4d275909e46ba1e27bf882d28
Rinkit Adhana <therinkit@gmail.com>
2025-06-28 02:16:19 +0530

    updated

---
+++ frontend/src/app/(main)/space/layout.tsx
@@ -0,0 +1,18 @@
+import { SocketProvider } from "@/context/socket";
+import React from "react";
+
+interface VideoCallLayoutProps {
+  children: React.ReactNode; 
+}
+
+const VideoCallLayout: React.FC<VideoCallLayoutProps> = ({ children }) => {
+  return (
+    <SocketProvider>
+      <div>
+        {children}
+      </div>
+    </SocketProvider>
+  );
+};
+
+export default VideoCallLayout;
