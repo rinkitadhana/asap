b48c007053770b853a2ac16a0fbf99a971d55640
Rinkit Adhana <therinkit@gmail.com>
2025-07-31 02:01:35 +0530

    improved the ui of controlls

--- frontend/src/app/(main)/space/components/Controls.tsx
+++ frontend/src/app/(main)/space/components/Controls.tsx
@@ -1,28 +1,29 @@
 import { BsFillRecordCircleFill, BsFillTelephoneFill } from "react-icons/bs"
 import { RiMic2Line } from "react-icons/ri"
 import { FiVideo } from "react-icons/fi"
-import { PiSpeakerHigh } from "react-icons/pi"
 import { LuScreenShare } from "react-icons/lu"
+import { RxSpeakerLoud } from "react-icons/rx"
+
 const Controls = () => {
   return (
-    <div className="flex items-center gap-2 rounded-xl bg-secondary/60 backdrop-blur-sm p-2">
-      <button className="flex gap-1.5 items-center justify-center px-3 h-10 rounded-[10px] bg-red-400 text-white font-medium text-sm cursor-pointer hover:bg-red-400/80 transition-all duration-200">
+    <div className="select-none flex items-center gap-2 p-2 my-1">
+      <button className="flex gap-1.5 items-center justify-center py-3 px-4 rounded-xl bg-red-400 text-white font-medium text-sm cursor-pointer hover:bg-red-400/80 transition-all duration-200">
         <BsFillRecordCircleFill className="text-base" /> <span>Record</span>
       </button>
-      <button className="flex items-center justify-center px-3 h-10 rounded-[10px] bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
+      <button className="flex items-center justify-center  border border-primary-border p-3 rounded-xl bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
         <RiMic2Line />
       </button>
-      <button className="flex items-center justify-center px-3 h-10 rounded-[10px] bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
+      <button className="flex items-center justify-center border border-primary-border p-3 rounded-xl bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
         <FiVideo />
       </button>
-      <button className="flex items-center justify-center px-3 h-10 rounded-[10px] bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
-        <PiSpeakerHigh />
+      <button className="flex items-center justify-center border border-primary-border p-3 rounded-xl bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
+        <RxSpeakerLoud />
       </button>
-      <button className="flex items-center justify-center px-3 h-10 rounded-[10px] bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
+      <button className="flex items-center justify-center border border-primary-border p-3 rounded-xl bg-primary-hover text-lg font-medium cursor-pointer hover:bg-primary-hover/80 transition-all duration-200">
         <LuScreenShare />
       </button>
-      <div className="h-6 border-r border-primary-border px-1" />
-      <button className="flex items-center justify-center px-3 h-10 rounded-[10px] text-lg font-medium cursor-pointer text-red-400 bg-red-400/20 hover:bg-red-400/30 transition-all duration-200">
+      <div className="h-8 border-r border-primary-border mx-1" />
+      <button className="flex items-center justify-center border border-red-400/10 p-3 rounded-xl text-lg font-medium cursor-pointer text-red-400 bg-red-400/20 hover:bg-red-400/30 transition-all duration-200">
         <BsFillTelephoneFill className="-rotate-[225deg]" />
       </button>
     </div>
--- frontend/src/app/(main)/space/components/SpaceWrapper.tsx
+++ frontend/src/app/(main)/space/components/SpaceWrapper.tsx
@@ -1,37 +1,19 @@
-import React, { useState } from "react"
+import React from "react"
 import Back from "@/components/Back"
 import Logo from "@/components/Logo"
 import { ThemeSwitcher } from "@/components/ThemeSwitcher"
-import { Lock, LockOpen, UserPlus } from "lucide-react"
-import DateTimeDisplay from "@/utils/Date"
+import { UserPlus } from "lucide-react"
 import Controls from "./Controls"
 import { LuUsers } from "react-icons/lu"
 import { IoChatbubbleOutline } from "react-icons/io5"
 import { BsInfoLg } from "react-icons/bs"
 
 const SpaceWrapper = ({ children }: { children: React.ReactNode }) => {
-  const [hidden, setHidden] = useState(false)
-
   return (
     <section className="bg-secondary h-screen flex items-center p-2">
       <div className="relative flex-1 flex flex-col items-center justify-center h-full max-w-full overflow-hidden">
-        <header
-          className={`w-full p-2 pl-0 ${hidden ? "absolute top-0 z-50 group h-14" : ""
-            }`}
-        >
-          {hidden && (
-            <div className="h-3 w-full absolute top-0 left-0 group-hover:cursor-pointer" />
-          )}
-          <div
-            className={`
-          flex items-center justify-between  p-2.5 w-full
-          transition-all duration-300 ease-in-out
-          ${hidden
-                ? "transform -translate-y-[calc(100%+8px)] group-hover:translate-y-0 bg-background rounded-t-xl border border-primary-border "
-                : "rounded-xl border border-primary-border bg-background  "
-              }
-        `}
-          >
+        <div className="w-full p-2 pl-0">
+          <div className="flex items-center justify-between p-2.5 w-full rounded-xl border border-primary-border bg-background">
             <div className="flex items-center gap-2">
               <Back />
               <Logo />
@@ -40,38 +22,23 @@ const SpaceWrapper = ({ children }: { children: React.ReactNode }) => {
                 Gruz's Space
               </div>
             </div>
-            <div className=" flex gap-2 items-center">
+            <div className="flex items-center gap-2">
               <div className="flex items-center gap-2 py-[0.57rem] px-3 rounded-md hover:bg-primary-hover/80 transition-all duration-200 bg-primary-hover cursor-pointer select-none">
                 <UserPlus size={17} />
                 <span className="font-medium text-[15px] leading-tight">
                   Invite
                 </span>
               </div>
-              <div
-                title={hidden ? "Lock" : "Unlock"}
-                onClick={() => setHidden(!hidden)}
-                className="flex items-center p-2.5 rounded-md hover:bg-primary-hover/80 transition-all duration-200 bg-primary-hover cursor-pointer select-none"
-              >
-                {hidden ? <LockOpen size={17} /> : <Lock size={17} />}
-              </div>
             </div>
           </div>
-        </header>
-        <div
-          className={`w-full flex-1 min-h-0 ${hidden ? "p-2 pl-0" : "px-2 pb-2 pl-0"}`}
-        >
+        </div>
+        <div className="w-full flex-1 min-h-0 px-2 pb-2 pl-0">
           {children}
         </div>
-        {/* <div className="fixed bottom-0 w-full h-24 group">
-          <div className="opacity-0 group-hover:opacity-100 transform translate-y-4 group-hover:translate-y-0 transition-all duration-200 ease-in-out fixed bottom-6 left-6 items-center gap-2 flex">
-            <DateTimeDisplay />
-            <div className="h-5 border-r border-primary-border px-1" />
-            <div>YUMNSK-KSM</div>
-          </div>
-          <div className="opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0 transition-all duration-200 ease-in-out absolute bottom-6 left-1/2 transform -translate-x-1/2 flex">
-            <Controls />
-          </div>
-        </div> */}
+        <div>
+          <Controls />
+        </div>
+
       </div>
       <div className="flex justify-center items-center h-full gap-2 flex-shrink-0">
         <div className=" flex flex-col justify-center items-center border border-primary-border h-full w-[200px] rounded-xl bg-background">
