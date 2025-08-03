d98da82c82aa500c23290beb2a71aa0bdc374310
Rinkit Adhana <therinkit@gmail.com>
2025-08-04 00:57:53 +0530

    temp box added

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero"
 import PageHead from "@/components/PageHead"
 import Stack from "@/components/Stack"
 import Footer from "@/components/Footer"
+import TempBox from "@/components/TempBox"
 const page = () => {
   return (
     <>
@@ -12,6 +13,10 @@ const page = () => {
       <Hero />
       <div className="border-t-2 border-main-border border-dashed w-full" />
       <Stack />
+      <div className="border-t-2 border-main-border border-dashed w-full" />
+      <TempBox />
+      <div className="border-t-2 border-main-border border-dashed w-full" />
+      <TempBox />
       <div className="border-t-2 border-main-border border-dashed w-full" />
       <Footer />
     </>
---
+++ frontend/src/components/TempBox.tsx
@@ -0,0 +1,15 @@
+import LayoutWrapper from '@/layout/LayoutWrapper'
+import React from 'react'
+
+const TempBox = () => {
+    return (
+        <LayoutWrapper>
+            <div className='border-x-2 border-primary-border border-dashed p-4'>
+                <div className='flex  flex-col min-h-[200px]  shadow-md shadow-primary-shadow justify-between w-full border border-primary-border rounded-xl p-8 bg-secondary'>
+                </div>
+            </div>
+        </LayoutWrapper>
+    )
+}
+
+export default TempBox
