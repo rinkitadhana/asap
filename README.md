9271ce10c647a99cf57c8173cf50ca00b999b231
Rinkit Adhana <therinkit@gmail.com>
2025-09-14 04:40:36 +0530

    setup Turborepo monorepo structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# ASAP
+- join asap call
---
+++ apps/core/src/index.ts
@@ -0,0 +1,27 @@
+import express, { type Request, type Response } from "express"
+import { createServer } from "http"
+import cors from "cors"
+import { initSocket } from "./sockets/index.ts"
+
+const app = express()
+const PORT = 4000
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}))
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("bordre is live :D")
+})
+
+
+const httpServer = createServer(app)
+
+initSocket(httpServer)
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} :D`)
+})
