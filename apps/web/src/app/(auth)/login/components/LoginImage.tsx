d49e9d5af4fc29910096980cb5b92bc778bb9326
Rinkit Adhana <therinkit@gmail.com>
2025-10-05 20:18:18 +0530

    add contact us to login page

--- apps/web/src/app/(auth)/login/components/LoginImage.tsx
+++ apps/web/src/app/(auth)/login/components/LoginImage.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
+import { RiCustomerService2Line } from 'react-icons/ri'
 
 const LoginImage = () => {
   return (
-    <Image className="select-none" src="/img/login/login-image.png" alt="login-image" width={500} height={500} />
+    <div className="flex flex-col gap-4 relative">
+        <Image className="select-none" src="/img/login/login-image.png" alt="login-image" width={500} height={500} />
+        <a href='mailto:contact@asap.com' className=" select-none flex flex-row items-center gap-2 absolute bottom-2 right-2 px-2 py-1 bg-background/40 hover:bg-background/55 transition-all duration-200 backdrop-blur-xs">
+        <RiCustomerService2Line size={16} />
+        Contact Us
+        </a>
+    </div>
   )
 }
 
