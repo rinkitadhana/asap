54c12a33d653b72b73a750526890fbdebfcb3310
Rinkit Adhana <therinkit@gmail.com>
2025-10-10 15:14:15 +0530

    improve login page

--- apps/web/src/app/(auth)/login/components/LoginImage.tsx
+++ apps/web/src/app/(auth)/login/components/LoginImage.tsx
@@ -4,12 +4,16 @@ import { RiCustomerService2Line } from 'react-icons/ri'
 
 const LoginImage = () => {
   return (
-    <div className="flex flex-col gap-4 relative border border-primary-border h-full overflow-hidden">
+    <div className="relative h-full overflow-visible">
+      <div className="absolute -top-0.5 -left-0.5 w-8 h-8 border-l-2 border-t-2 border-foreground z-10" />
+      <div className="absolute -bottom-0.5 -right-0.5 w-8 h-8 border-r-2 border-b-2 border-foreground z-10" />
+      <div className="flex flex-col gap-4 relative h-full overflow-hidden">
         <Image className="select-none" src="/img/login/login-image.png" alt="login-image" width={500} height={500} />
         <a href='mailto:therinkit@gmail.com' className=" select-none flex flex-row items-center gap-2 absolute bottom-2 right-2 px-2 py-1 bg-background/40 hover:bg-background/55 transition-all duration-200 backdrop-blur-xs">
-        <RiCustomerService2Line size={16} />
-        Contact Us
+          <RiCustomerService2Line size={16} />
+          Contact Us
         </a>
+      </div>
     </div>
   )
 }
--- apps/web/src/app/(auth)/login/components/LoginLogo.tsx
+++ apps/web/src/app/(auth)/login/components/LoginLogo.tsx
@@ -4,7 +4,7 @@ const LoginLogo = () => {
   return (
     <div className="flex flex-col ">
     <h1 className="text-2xl font-medium">Asap</h1>
-    <p className="text-sm text-secondary-text">High-quality video calls for creators and storytellers</p>
+    <p className="text-sm text-secondary-text">High-quality video calls for creators and storytellers.</p>
     </div>
   )
 }
