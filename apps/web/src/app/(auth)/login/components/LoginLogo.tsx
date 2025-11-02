3f3890aacab7fb02c74acc99d8f51a8149b4a749
Rinkit Adhana <therinkit@gmail.com>
2025-11-02 15:23:48 +0530

    logo fixes

--- apps/web/src/app/(auth)/login/components/LoginLogo.tsx
+++ apps/web/src/app/(auth)/login/components/LoginLogo.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const LoginLogo = () => {
   return (
-    <div className="flex flex-col ">
-      <h1 className="text-2xl font-medium">Asap</h1>
+    <div className="flex flex-col gap-0.5">
+      <h1 className="text-2xl font-bold">Asap</h1>
       <p className="text-sm text-secondary-text">
         High-quality video calls for creators and storytellers.
       </p>
--- apps/web/src/shared/components/ui/AsapLogo.tsx
+++ apps/web/src/shared/components/ui/AsapLogo.tsx
@@ -1,20 +1,30 @@
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AsapLogo = ({ icon, name }: { icon?: boolean, name?: boolean }) => {
-  const { theme } = useTheme();
-  const darkMode = theme === "dark";
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const darkMode = resolvedTheme === "dark";
+
   return (
     <div className="flex items-center gap-2">
-      {icon && (
+      {icon && mounted && (
         <Image
-          src= {darkMode ? "/img/logo/rounded-logo-dark.png" : "/img/logo/rounded-logo-light.png"}
-          className="size-[32px]"
+          src={darkMode ? "/logo/rounded-logo-dark.png" : "/logo/rounded-logo-light.png"}
+          className="size-[32px] select-none"
           alt="Asap"
           width={40}
           height={40}
         />
+      )}
+      {icon && !mounted && (
+        <div className="size-[32px]" />
       )}
       {
         name && (
