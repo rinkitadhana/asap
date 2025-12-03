3b12bfb5fe347b2a89c54a4b8b7cf57fa76d99ae
Rinkit Adhana <therinkit@gmail.com>
2025-12-03 07:47:51 +0530

    add participant routes

---
+++ apps/core/src/routes/participant-route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { authMiddleware, optionalAuthMiddleware } from "../middlewares/auth-middleware";
+import {
+  joinSpaceController,
+  leaveSpaceController,
+  getParticipantsController,
+  updateRoleController,
+  kickParticipantController,
+} from "../controllers/participant-controller";
+
+const router = express.Router();
+
+router.post("/:spaceId/join", optionalAuthMiddleware, joinSpaceController);
+router.post("/:spaceId/leave", optionalAuthMiddleware, leaveSpaceController);
+router.get("/:spaceId/list", getParticipantsController);
+router.patch("/:spaceId/participant/:participantId/role", authMiddleware, updateRoleController);
+router.post("/:spaceId/participant/:participantId/kick", authMiddleware, kickParticipantController);
+
+export default router;
+
